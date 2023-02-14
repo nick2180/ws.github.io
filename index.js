46e1de1792d62088eae26bbb254d70de1b3f41fd
Nicolas BLANC <nicolasblanc46@gmail.com>
2023-02-14 20:35:01 +0100

    com

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ const WebSocket = require("ws");
 
 const wss = new WebSocket.Server({ port: 3000 });
 
-wss.on("connection", ws => {
+wss.on("connection", (ws, req) => {
+    const origin = req.headers.origin;
+
+    // Allow all origins
+    if (origin !== "*") {
+        console.log("Invalid origin:", origin);
+        ws.close();
+        return;
+    }
+
     console.log("Client connected to server");
-    ws.on("message", message => {
+    ws.on("message", (message) => {
         console.log("Message received from client: " + message);
         ws.send("Echo message sent back from server: " + message);
     });
-    ws.send("Hello! Message from server. You've just connected to the server!!");
+    ws.send(
+        "Hello! Message from server. You've just connected to the server!!"
+    );
 });
-
-
