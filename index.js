56b50872bc726a0bab883c51d7310f5009b99306
Nicolas BLANC <nicolasblanc46@gmail.com>
2023-02-14 12:03:35 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on("connection", ws => {
+    console.log("Client connected to server");
+    ws.on("message", message => {
+        console.log("Message received from client: " + message);
+        ws.send("Echo message sent back from server: " + message);
+    });
+    ws.send("Hello! Message from server. You've just connected to the server!!");
+});
