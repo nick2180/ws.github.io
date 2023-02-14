c6f421c0e61e322ad0247ed1e491a2db5842ec4f
Nicolas BLANC <nicolasblanc46@gmail.com>
2023-02-14 21:16:06 +0100

    com

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:latest
+
+# Create app directory
+WORKDIR /usr/src/app
+
+# Install app dependencies
+COPY package*.json ./
+RUN npm install
+
+# Bundle app source
+COPY . .
+
+# Expose port
+EXPOSE 8080
+
+# Run the command to start your app
+CMD [ "npm", "start" ]
--- index.js
+++ index.js
@@ -1,23 +1,13 @@
+
 const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({ port: 3000 });
+const wss = new WebSocket.Server({ port: 8080 });
 
-wss.on("connection", (ws, req) => {
-    const origin = req.headers.origin;
-
-    // Allow all origins
-    if (origin !== "*") {
-        console.log("Invalid origin:", origin);
-        ws.close();
-        return;
-    }
-
+wss.on("connection", ws => {
     console.log("Client connected to server");
-    ws.on("message", (message) => {
+    ws.on("message", message => {
         console.log("Message received from client: " + message);
         ws.send("Echo message sent back from server: " + message);
     });
-    ws.send(
-        "Hello! Message from server. You've just connected to the server!!"
-    );
+    ws.send("Hello! Message from server. You've just connected to the server!!");
 });
